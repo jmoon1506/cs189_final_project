7861e03605dbc94454b8e1573d8899288db6f38f
Chang Chu-Ming <changchuming@gmail.com>
2017-12-06 16:03:39 +0000

    testing on ec2

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class LatentAttention():
         self.n_hidden = 500
         self.n_z = 20
         self.batchsize = 100
+        self.step_size = 0.0001
+        self.epoch = 10000
 
         self.images = tf.placeholder(tf.float32, [None, 16384])
         image_matrix = tf.reshape(self.images,[-1, 128, 128, 1])
@@ -30,31 +32,35 @@ class LatentAttention():
 
         self.latent_loss = 0.5 * tf.reduce_sum(tf.square(z_mean) + tf.square(z_stddev) - tf.log(tf.square(z_stddev)) - 1,1)
         self.cost = tf.reduce_mean(self.generation_loss + self.latent_loss)
-        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.cost)
+        self.optimizer = tf.train.AdamOptimizer(self.step_size).minimize(self.cost)
 
 
     # encoder
     def recognition(self, input_images):
         with tf.variable_scope("recognition"):
-            h1 = lrelu(conv2d(input_images, 1, 16, "d_h1")) # 28x28x1 -> 14x14x16
-            h2 = lrelu(conv2d(h1, 16, 32, "d_h2")) # 14x14x16 -> 7x7x32
-            h2_flat = tf.reshape(h2,[self.batchsize,32*32*32])
+            h1 = lrelu(conv2d(input_images, 1, 16, "d_h1")) # 128x128x1 -> 64x64x16
+            h2 = lrelu(conv2d(h1, 16, 32, "d_h2")) # 64x64x16 -> 32x32x32
+            h3 = lrelu(conv2d(h2, 32, 64, "d_h3")) # 32x32x32 -> 16x16x64
+            h4 = lrelu(conv2d(h3, 64, 128, "d_h4")) # 16x16x64 -> 8x8x128
+            h4_flat = tf.reshape(h4,[self.batchsize,8*8*128])
 
-            w_mean = dense(h2_flat, 32*32*32, self.n_z, "w_mean")
-            w_stddev = dense(h2_flat, 32*32*32, self.n_z, "w_stddev")
+            w_mean = dense(h4_flat, 8*8*128, self.n_z, "w_mean")
+            w_stddev = dense(h4_flat, 8*8*128, self.n_z, "w_stddev")
 
         return w_mean, w_stddev
 
     # decoder
     def generation(self, z):
         with tf.variable_scope("generation"):
-            z_develop = dense(z, self.n_z, 32*32*32, scope='z_matrix')
-            z_matrix = tf.nn.relu(tf.reshape(z_develop, [self.batchsize, 32, 32, 32]))
-            h1 = tf.nn.relu(conv_transpose(z_matrix, [self.batchsize, 64, 64, 16], "g_h1"))
-            h2 = conv_transpose(h1, [self.batchsize, 128, 128, 1], "g_h2")
-            h2 = tf.nn.sigmoid(h2)
+            z_develop = dense(z, self.n_z, 8*8*128, scope='z_matrix')
+            z_matrix = tf.nn.relu(tf.reshape(z_develop, [self.batchsize, 8, 8, 128]))
+            h1 = tf.nn.relu(conv_transpose(z_matrix, [self.batchsize, 16, 16, 64], "g_h1"))
+            h2 = tf.nn.relu(conv_transpose(h1, [self.batchsize, 32, 32, 32], "g_h2"))
+            h3 = tf.nn.relu(conv_transpose(h2, [self.batchsize, 64, 64, 16], "g_h3"))
+            h4 = conv_transpose(h3, [self.batchsize, 128, 128, 1], "g_h4")
+            h4 = tf.nn.sigmoid(h4)
 
-        return h2
+        return h4
 
     def train(self):
         visualization = self.mnist.train.next_batch(self.batchsize)[0]
@@ -63,9 +69,10 @@ class LatentAttention():
         ims("results/base.jpg",merge(reshaped_vis[:64],[8,8]))
         # train
         saver = tf.train.Saver(max_to_keep=2)
-        with tf.Session() as sess:
+        with tf.Session() as  sess:
+        #with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
             sess.run(tf.initialize_all_variables())
-            for epoch in range(200):
+            for epoch in range(self.epoch):
                 for idx in range(int(self.n_samples / self.batchsize)):
                     batch = self.mnist.train.next_batch(self.batchsize)[0]
                     _, gen_loss, lat_loss = sess.run((self.optimizer, self.generation_loss, self.latent_loss), feed_dict={self.images: batch})
